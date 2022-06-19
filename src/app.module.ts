df2907cc5a540cb35be593e49a5090b587f38b55
Alvaro2003j <u202015074@upc.edu.pe>
2022-06-19 14:05:24 -0500

    implement-suscription-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,36 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from './clients/clients.module';
+import { SuscriptionsModule } from './suscriptions/suscriptions.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountsModule } from './accounts/accounts.module';
 import { TransactionsModule } from './transactions/transactions.module';
-
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      url: process.env.BANKING_DDD_NEST_MYSQL,
+      migrationsRun: true,
+      logging: true,
+      timezone: '+00:00',
+      bigNumberStrings: false,
+      entities: [
+        process.env.ENVIROMENT == 'prod' ?
+        '**/infrastructure/persistence/typeorm/entities/*{.ts,.js}' :
+        'dist/**/infrastructure/persistence/typeorm/entities/*{.ts, .js}'
+      ],
+      subscribers: [],
+      migrations: [
+        process.env.ENVIROMENT == 'prod' ?
+        'common/infrastructure/persistence/typeorm/migrations/*{.ts,.js}' :
+        'dist/common/infrastructure/persistence/typeorm/migrations/*{.ts,.js}'
+      ],
+      migrationsTableName: "migrations"
+    }),
     ClientsModule,
     AccountsModule,
-    TransactionsModule,
-    TypeOrmModule.forRoot()
+    SuscriptionsModule,
+    TransactionsModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/suscriptions/suscriptions.module.ts
+++ src/suscriptions/suscriptions.module.ts
@@ -3,3 +3,28 @@ import { SuscriptionController } from './api/suscriptions.controller';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MoneyTransferredHandler } from './application/handlers/events/money-transferred.handler';
+import { RegisterSuscriptionHandler } from './applicaction/handlers/commands/register-suscription.handler';
+import { SuscriptionRegisteredHandler } from './applicaction/handlers/events/suscription-registered.handler';
+import { SuscriptionTypeORM } from './infrastructure/persistence/typeorm/entities/suscription.typeorm';
+import { SuscriptionApplicationService } from './applicaction/services/suscription-application.service';
+import { RegisterSuscriptionValidator } from './applicaction/validators/registered-suscription.validator';
+
+export const CommandHandlers = [RegisterSuscriptionHandler];
+export const EventHandlers = [SuscriptionRegisteredHandler];
+export const QueryHandlers = [];
+
+@Module({
+    imports: [
+        CqrsModule,
+        TypeOrmModule.forFeature([SuscriptionTypeORM]),
+    ],
+    controllers: [SuscriptionController],
+    providers: [
+        SuscriptionApplicationService,
+        RegisterSuscriptionValidator,
+        ...CommandHandlers,
+        ...EventHandlers,
+        ...QueryHandlers
+    ]
+})
+export class SuscriptionsModule {}
