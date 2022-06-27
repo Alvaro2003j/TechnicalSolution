b1b9764e35a8069382c665ef573bf6f0f32bfccf
Alvaro2003j <u202015074@upc.edu.pe>
2022-06-27 01:04:44 -0500

    update-initial-schemas-ql

--- src/common/infrastructure/persistence/typeorm/migrations/new-initial-schema.sql
+++ src/common/infrastructure/persistence/typeorm/migrations/new-initial-schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS suscriptions(
   id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   cost DECIMAL(10, 2) NULL,
-  active BOOLEAN NOT NULL DEFAULT 0,
-  time_sucription_days INT NOT NULL, 
+  active BOOLEAN NULL DEFAULT 0,
+  time_suscription_days INT NULL, 
   type_suscription VARCHAR(10) NULL,
   PRIMARY KEY(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
